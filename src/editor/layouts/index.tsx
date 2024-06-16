3bccc7975d3ffa5d9c37d2c75f79a24254c08c99
Larissa <1781476738@qq.com>
2024-06-16 14:45:48 +0800

    feat: layout 布局

--- src/editor/layouts/index.tsx
+++ src/editor/layouts/index.tsx
@@ -3,15 +3,25 @@ import Header from './header';
 import Material from './material';
 import Stage from './stage';
 import Setting from './setting';
-
+import "./index.scss"
 
 function Layout(){
     return(
-      <div>
-        <Header/>
-        <Material/>
-        <Stage/>
-        <Setting/>
+      <div className="layout">
+        <div className='header'>
+          <Header/>
+        </div>
+        <div className='content'>
+          <div className='left'>
+            <Material/>
+          </div>
+          <div className='box'>
+            <Stage/>
+          </div>
+          <div className='right'>
+            <Setting/>
+          </div>
+        </div>
       </div>
     )
   }
